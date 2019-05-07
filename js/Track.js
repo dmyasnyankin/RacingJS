6b5ccea59f90b29575bb47667cf80c1844adc98a
Dennis Myasnyankin <dennis415@live.com>
2019-05-07 00:27:08 -0700

    Optimize frame rendering

--- js/Track.js
+++ js/Track.js
@@ -54,14 +54,20 @@ function rowColToArrayIndex(col, row) {
 
 function drawTracks() {
 
+	var arrayIndex = 0;
+	var drawTileX = 0;
+	var drawTileY = 0;
 	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
 		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
-
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
 			var tileKindPresent = trackGrid[arrayIndex];
 			var useImg = trackPics[tileKindPresent];
-			canvasContext.drawImage(useImg, TRACK_W*eachCol, TRACK_H*eachRow);
+			canvasContext.drawImage(useImg, drawTileX, drawTileY);
+
+			drawTileX += TRACK_W;
+			arrayIndex += 1;
 		} // end of for each col
+		drawTileY += TRACK_H;
+		drawTileX = 0;
 	} // end of for each row
 
 } // end of drawTracks func
